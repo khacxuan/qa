8d8faa9997e18f69968c1ba5fa038a967fe65fb7
haudv <haudv@cpu-haudv.(none)>
2013-10-09 17:21:34 +0700

    Hau commit tag list

--- fuel/app/classes/model/user/tag.php
+++ fuel/app/classes/model/user/tag.php
@@ -47,33 +47,48 @@ class Model_User_Tag {
 		return $result;
 	}
 	
-	public static function getTagList($tag_id = "", $limit, $offset) {
 	
-		if ($tag_id != "") {
-			$con = '{tag_ids: ObjectId("' . $tag_id . '")}';
+	public static function getTagTotal($key = "") {
+		if ($key != "") {
+			$key = preg_quote($key);
+			$key = str_replace(" ","|" ,$key);
+			$key = str_replace("　","|" ,$key);
+			$con = '{name:{ $regex: "'.$key.'",$options: "im"}}';
 		} else {
 			$con = "";
 		}
 	
 		$mdb = Mongo_DB::instance();
 		$result = $mdb->execute('function (){
-				var a = [];
-				db.qa.find(' . $con . ').skip(' . $offset . ').limit(' . $limit . ').sort( { created_at: -1 } ).forEach(function(w){
-				var b = [];
-				if(w.tag_ids){
-				b = db.tags.find({_id:{$in:w.tag_ids}}).toArray();
-	}
-				a.push({
-				qa:w,
-				tag:b
-	});
-	});
-				return a;
-	}');
+				return db.tags.find(' . $con .').count()
+			}');
 	
-		return $result;
+		if($result['ok'] == 1 && !empty($result['retval'])){
+			return $result['retval'];
+		}
+		return 0;	
 	}
-
+	
+	public static function getTagList($key = "", $limit, $offset) {
+		if ($key != "") {
+			$key = preg_quote($key);
+			$key = str_replace(" ","|" ,$key);
+			$key = str_replace("　","|" ,$key);
+			$con = '{name:{ $regex: "'.$key.'",$options: "im"}}';
+		} else {
+			$con = "";
+		}
+		
+		$mdb = Mongo_DB::instance();
+		$result = $mdb->execute('function (){
+			return db.tags.find(' . $con .').skip(' . $offset . ').limit(' . $limit . ').toArray();
+		}');
+			
+		if($result['ok'] == 1 && !empty($result['retval'])){
+			return $result['retval'];
+		}
+		return array();
+	}
 }
 ?>
 
