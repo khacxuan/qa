760468e0c1a1c5f51c021005b612c003e159bf42
lephuqui <lephuqui@localhost>
2013-10-09 15:48:57 +0700

    qui-test

--- fuel/app/views/user/list/index.php
+++ fuel/app/views/user/list/index.php
@@ -1,15 +1,15 @@
 <h2>List</h2>
 <!-- search form -->
-<form action="<?php echo uri::base().'user/list'?>" method="get">
+<form action="<?php echo uri::base() . 'user/list' ?>" method="get">
 	<div class="search form">
 		<table class="table table-bordered">
 			<tbody>
 				<tr>
 					<th>開始日</th>
 					<td>
-						<label><input class="w100p" id="key" name="key" value="<?php echo trim(Input::param('key','')," 　\t\n\r\0\x0B") ?>"></label>
+						<label><input class="w100p" id="key" name="key" value="<?php echo trim(Input::param('key', ''), " 　\t\n\r\0\x0B") ?>"></label>
 					</td>
-					
+
 				</tr>
 				<tr>					
 					<td colspan="2">
@@ -22,7 +22,7 @@
 </form>
 <!-- END search form -->
 <a href="<?php echo Uri::create('user/newqa') ?>">質問を追加</a>
-<?php if ($data['ok']==1&& !empty($data['retval'])) {  ?>
+<?php if ($data['ok'] == 1 && !empty($data['retval'])) { ?>
 	<table class="table table-bordered table-data admin-com-index" border="1">
 		<tbody><tr>
 				<th>Title</th>
@@ -30,39 +30,41 @@
 				<th>Tags</th>
 				<th>Date</th>
 			</tr>
-			<?php 
-			$data = $data['retval'];			
-			foreach ($data as $dt) { ?>
+			<?php
+			$data = $data['retval'];
+			foreach ($data as $dt) {
+				?>
 				<tr>
-					<td><a href="<?php echo uri::base().'user/detail/'.$dt['qa']['_id']; ?>"><?php echo @$dt['qa']['question_title']; ?></a></td>
+					<td><a href="<?php echo uri::base() . 'user/detail/' . $dt['qa']['_id']; ?>"><?php echo @$dt['qa']['question_title']; ?></a></td>
 					<td><?php echo @$dt['qa']['question_content']; ?></td>
-					<td><?php foreach($dt['tag'] as $tag){ ?> 
-						<a href="<?php echo uri::base().'user/tag/'.$tag['_id']; ?>">
+					<td><?php foreach ($dt['tag'] as $tag) { ?> 
+							<a href="<?php echo uri::base() . 'user/tag/' . $tag['_id']; ?>">
 							<?php echo $tag['name']; ?>
-						</a>&nbsp;&nbsp;&nbsp;&nbsp;
-						<?php } ?>
+							</a>&nbsp;&nbsp;&nbsp;&nbsp;
+		<?php } ?>
 					</td>
-					<td><?php  echo  (@$dt['qa']['created_at']!="")?date('Y-m-d',@$dt['qa']['created_at']):""; ?></td>
-					
+					<td><?php echo (@$dt['qa']['created_at'] != "") ? date('Y-m-d', @$dt['qa']['created_at']) : ""; ?></td>
+
 				</tr>
 
-			<?php } ?>
+	<?php } ?>
 		</tbody></table>
+	<div class="page-nav row">
+		<div class="three columns">
+			<?php if (isset($total_record) and $total_record > 0): ?>
+				<?php echo isset($page_from) ? $page_from : '0' ?> ∼ <?php echo isset($page_to) ? $page_to : '0' ?>/<?php echo isset($students_count) ? $students_count : '0' ?> 件
+		<?php endif; ?>
+		</div>
+	<?php if ($total_record > $total_record_page) { ?>
+			<div class="pagination-holder nine columns" id="pagination_code">
+			</div>
+	<?php } ?>
+	</div>
 <?php } else { ?>
 	<div class="message">該当データがありません。</div>
 <?php } ?>
 
-<div class="page-nav row">
-	<div class="three columns">
-		<?php if (isset($total_record) and $total_record > 0): ?>
-			<?php echo isset($page_from) ? $page_from : '0' ?> ∼ <?php echo isset($page_to) ? $page_to : '0' ?>/<?php echo isset($students_count) ? $students_count : '0' ?> 件
-		<?php endif; ?>
-	</div>
-	<?php if ($total_record > $total_record_page) { ?>
-		<div class="pagination-holder nine columns" id="pagination_code">
-		</div>
-	<?php } ?>
-</div>
+
 <script type="text/javascript">
 	$(document).ready(function() {
 
