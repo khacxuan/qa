c808cb20eb0ecf19656f76abb0e3be3707e2abe6
lkxuan <xuanlk@evolable.asia>
2013-11-07 09:51:49 +0700

    item on page

--- fuel/app/config/common.php
+++ fuel/app/config/common.php
@@ -4,8 +4,8 @@ define('SESSION_QA_ADMIN', 'qaAdmin');
 
 return array(
 	'qa_confirm_delete' => '削除しても宜しいですか？',
-	'listuser_limit' => 2,
-	'listuser_displayed_pages' => 2,
+	'listuser_limit' => 10,
+	'listuser_displayed_pages' => 10,
 	'qa_record_page_count' => 10,
 	'qa_paging_displayed_pages' => 10,
 	'flag_social' => array('facebook' => 1, 'twitter' => 2, 'github' => 3, 'none' => 4),
